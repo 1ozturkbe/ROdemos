7399dda21cd0fdbcef8d90c17596d5cc37676f4c
1ozturkbe <1ozturkbe@gmail.com>
2022-03-11 16:47:32 +0000

    solution and dump separated.

--- homeworks/HW4/solution.jl
+++ homeworks/HW4/solution.jl
@@ -4,6 +4,8 @@ Pkg.activate(".")
 
 # Packages
 using JuMP, Gurobi, Random, Distributions, LinearAlgebra, DataFrames, Plots
+# GUROBI_SILENT = with_optimizer(Gurobi.Optimizer, OutputFlag = 0, Gurobi.Env())
+GUROBI_SILENT = Gurobi.Optimizer
 
 n = 10 # Number of facilities
 m = 50 # Number of customers
@@ -25,7 +27,7 @@ P = (P .>= 0.2*exp(-1/R_D .* R_D)) .* P
 function facility_model(c::Matrix, f::Vector)
     n, m = size(c) 
     @assert length(f) == n
-    model = Model(solver = GurobiSolver())
+    model = Model(GUROBI_SILENT)
 
     # VARIABLES
     @variable(model, x[1:n], Bin)      # Facility locations
@@ -39,33 +41,11 @@ function facility_model(c::Matrix, f::Vector)
     return model, x, y
 end
 
-""" Robust facility location model, with JuMPeR. """
-function robust_facility_model(c::Matrix, f::Vector, rho::Real, Gamm::Real)
-    n, m = size(c) 
-    @assert length(f) == n
-    model = RobustModel(solver = GurobiSolver())
-    # model = Model(solver = GurobiSolver())
-
-    # VARIABLES
-    @variable(model, x[1:n], Bin)                 # Facility locations
-    @variable(model, y[1:n, 1:m] >= 0)            # Flow variables
-    @uncertain(model, -rho <= z[1:m] <= rho)      #  Demand uncertainties
-    @constraint(model, norm(z, 1) <= Gamm)
-
-    # CONSTRAINTS
-    @constraint(model, [j=1:m], sum(y[i, j] for i = 1:n) >= d[j] + (P*z)[j]) # demand constraint
-    @constraint(model, [i=1:n], sum(y[i, j] for j = 1:m) <= s[i] * x[i]) # capacity constraint
-    # OBJECTIVE
-    @objective(model, Min, 
-            sum(f[j] * x[j] for j = 1:n) + sum(c[i, j] * y[i, j] for i=1:n, j=1:m))
-    return model, x, y
-end
-
 """ Robust facility location model, with JuMP using the robust counterpart. """
 function robust_facility_model(c::Matrix, f::Vector, rho::Real, Gamm::Real)
     n, m = size(c) 
     @assert length(f) == n
-    model = Model(solver = GurobiSolver())
+    model = Model(GUROBI_SILENT)
 
     # VARIABLES
     @variable(model, x[1:n], Bin)                 # Facility locations
@@ -87,28 +67,6 @@ function robust_facility_model(c::Matrix, f::Vector, rho::Real, Gamm::Real)
     @objective(model, Min, 
             sum(f[j] * x[j] for j = 1:n) + sum(c[i, j] * u[i, j] for i=1:n, j=1:m))
     return model, x, u
-end
-
-""" Adaptive facility location model, with JuMPeR and relaxation of binary variables. """
-function adaptive_facility_model(c::Matrix, f::Vector, rho::Real, Gamm::Real)
-    n, m = size(c) 
-    @assert length(f) == n
-    model = RobustModel(solver = GurobiSolver())
-
-    # VARIABLES
-    @variable(model, 0 <= x[1:n] <= 1)                 # Facility locations
-    @uncertain(model, -rho <= z[1:m] <= rho)      #  Demand uncertainties
-    @constraint(model, norm(z, 1) <= Gamm)
-    @adaptive(model, y[i=1:n, j=1:m] >= 0, policy = Affine, depends_on=z) # Flow variables
-
-    # CONSTRAINTS
-    @constraint(model, [j=1:m], sum(y[i, j] for i = 1:n) >= d[j] + (P*z)[j]) # demand constraint
-    @constraint(model, [i=1:n], sum(y[i, j] for j = 1:m) <= s[i] * x[i]) # capacity constraint
-    # OBJECTIVE
-    @variable(model, F)
-    @constraint(model, F >= sum(f[j] * x[j] for j = 1:n) + sum(c[i, j] * y[i, j] for i=1:n, j=1:m))
-    @objective(model, Min, F)
-    return model, x, y
 end
 
 """ Plots the solution of the facility location model. 
@@ -141,7 +99,7 @@ end
 
 # Nominal model (3.1)
 model, x, y = facility_model(c, f)
-solve(model)
+optimize!(model)
 plt = plot_solution(model, x, y)
 @show plt
 
@@ -149,41 +107,8 @@ plt = plot_solution(model, x, y)
 rho = 1
 Gamm = 5
 model, x, y = robust_facility_model(c, f, rho, Gamm)
-solve(model)
+optimize!(model)
 plt = plot_solution(model, x, y)
-@show plt
-
-# Adaptive solution with greedy binary heuristic (3.4 and 3.6)
-function iterate_adaptive_model(c, f, rho, Gamm)
-    ints = []
-    xvals = zeros(size(c, 1))
-    while !all([val in [0,1] for val in xvals]) || isempty(ints)
-        model, x, y = adaptive_facility_model(c, f, rho, Gamm)
-        for int in ints
-            @constraint(model, x[int] == 1)
-        end
-        solve(model)
-        xvals = value(x)
-        nonint = [!(var in [0,1]) for var in xvals] 
-        if sum(nonint) == 0
-            return model, x, y, ints
-        end
-        max_val, max_idx = findmax(nonint.*xvals)
-        push!(ints, max_idx)
-    end
-    return
-end
-
-model, x, y, ints = iterate_adaptive_model(c, f, rho, Gamm)
-
-# Plot generation for adaptive solution (3.4 and 3.6)
-cost = objective_value(model)
-nonints = [val for val in 1:n if !(val in ints)]
-plot_model, x, y = facility_model(c, f)
-@constraint(plot_model, x[ints] .== 1)
-@constraint(plot_model, x[nonints] .== 0)
-solve(plot_model)
-plt = plot_solution(model, x, y, cost)
 @show plt
 
 # Different radii of influence
@@ -197,7 +122,7 @@ for k=1:length(R_Ds)
     global P = (P .>= 0.2*exp(-1/R_Ds[k] .* R_Ds[k])) .* P
     println("Nonzero Ps: $(sum(P .> 0))")
     model, x, y = robust_facility_model(c, f, rho, Gamm)
-    solve(model)
+    optimize!(model)
     push!(df, Dict("R_D" => R_Ds[k],
                      "sumP" => sum(P),
                      "Gamma" => Gamm,
@@ -205,7 +130,7 @@ for k=1:length(R_Ds)
                      "f" => value(sum(f[j] * x[j] for j = 1:n)),
                      "c" => value(sum(c[i, j] * y[i, j] for i=1:n, j=1:m)),
                      "nP" => sum(P .> 0),
-                     "nx" => sum(value(x))))
+                     "nx" => sum(value.(x))))
 end
 
 # Column-wise scaled P (3.7)
@@ -220,7 +145,7 @@ for k=1:length(R_Ds)
     end
     println("Nonzero Ps: $(sum(P .> 0))")
     model, x, y = robust_facility_model(c, f, rho, Gamm)
-    solve(model)
+    optimize!(model)
     push!(df, Dict("R_D" => R_Ds[k],
                      "sumP" => sum(P),
                      "Gamma" => Gamm,
